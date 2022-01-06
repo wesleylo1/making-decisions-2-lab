34a03aedb87afb3d7ae098f22b440b20a53d0246
Wesley Lo <wesleylo1995@gmail.com>
2022-01-06 14:50:24 -0600

    Problem 5 solved

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -61,10 +61,21 @@ var score = 74
 // Do not edit the code above.
 
 /*
-  Determine if the letter grade of the given variable 'score'. If the variable is a 90 or above, console log an 'A', between 80 and 89, console log a 'B', between 70 and 79, 'C', between 60 and 69, 'D', and anything below 60 should console log an 'F'.
+  Determine if the letter grade of the given variable 'score'. If the variable is a 90 or above, console log an 'A', between 80 and 89, console log a 'B', 
+  between 70 and 79, 'C', between 60 and 69, 'D', and anything below 60 should console log an 'F'.
 */
 
-//Code Here
+if (score >= 90) {
+  console.log('A')
+} else if (score > 80 && score < 89) {
+  console.log('B')
+} else if (score > 70 && score < 79) {
+  console.log('C')
+} else if (score > 60 && score < 69) {
+  console.log('D')
+} else if (score < 60) {
+  console.log('F')
+}
 
 
 ////////// Intermediate Problems //////////
