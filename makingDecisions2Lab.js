73a7a655dac50daa1495d49e711e2e72d31a3373
Wesley Lo <wesleylo1995@gmail.com>
2022-01-06 15:31:33 -0600

    Problem 9 solved

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -132,7 +132,10 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
   Loop backwards, starting at the end of the 'letters' array. Console log every item in the array.
 */
 
-//Code Here
+for (let i = letters.length-1; i >= 0; i--) {
+  console.log(letters[i])
+}
+
 
 
 ////////// Advanced Problems //////////
